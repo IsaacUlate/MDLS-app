839dfb2f91cd90f8f432ce6c63b35902c0618c40
IsaacUlate <isaaculate2@gmail.com>
2024-03-27 01:12:20 -0600

    header-footer

--- MDLS-app/routes/web.php
+++ MDLS-app/routes/web.php
@@ -3,13 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/perfil', function () {
-    return view('perfil');
-});
-
-Route::get('/foro', function () {
-    return view('foro');
+    return view('principal');
 });
